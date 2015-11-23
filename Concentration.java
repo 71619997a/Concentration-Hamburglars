a9b37837ece2391a4e2913df9a59a5deb6f1d1ee
71619997a <gmarks@stuy.edu>
2015-11-23 17:37:18 -0500

    Constructor

--- Concentration.java
+++ Concentration.java
@@ -6,14 +6,25 @@ public class Concentration {
     //instance variables
     private Tile[][] _board;     //storage for 4x4 grid of Tiles.
     private int _numberFaceUp;   //count of Tiles with faces visible
-    private String[] _words;     //list of unique Strings used for Tile vals
+    private String[] _words = {"A","B","C","D","E","F","G","H","A","B","C","D","E","F","G","H"};     //list of unique Strings used for Tile vals
     private static int _numRows = 4;
 
-
     //insert constructor and methods here
-
-
-		
+    public Concentration() {
+	_numberFaceUp = 0;
+	_board = new Tile[_numRows][_numRows];
+	for(int i = 0;i < _numRows;i++) {
+	    for(int j = 0; j < _numRows;j++) {
+		int wordN = (int)(Math.random()*16);
+		while(!_words[wordN].equals(" ")) {
+		    wordN = (int)(Math.random()*16);
+		}
+		_board[i][j] = new Tile(_words[wordN]);
+		_words[wordN] = " "; //kinda sloppy but meh
+	    }
+	}
+    }
+    
     //DO NOT MODIFY main()
     public static void main(String[] args){
 	Concentration game = new Concentration();
